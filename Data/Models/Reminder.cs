11b3b9a7b15991f2cf75c36a3671f3c55e889e4d
Rizwan Saeedi <mrizwansaeedi3@gmail.com>
2022-11-29 19:10:32 +0500

    init

---
+++ Data/Models/Reminder.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication5.Data.Models
+{
+    public class Reminder
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime RemindAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
